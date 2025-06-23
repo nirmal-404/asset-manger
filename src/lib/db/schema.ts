828f912e9d0f033b5543ae5976bc572f8a984b42
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-06-24 01:25:20 +0530

    updated relations

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,70 +1,83 @@
-import { relations } from 'drizzle-orm'
-import { pgTable, text, timestamp, boolean, integer, serial, uuid } from 'drizzle-orm/pg-core'
+import { relations } from "drizzle-orm";
+import {
+  pgTable,
+  text,
+  timestamp,
+  boolean,
+  integer,
+  serial,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-export const user = pgTable('user', {
-  id: text('id').primaryKey(),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  emailVerified: boolean('email_verified')
+// user management tables
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified")
     .$defaultFn(() => false)
     .notNull(),
-  image: text('image'),
-  createdAt: timestamp('created_at')
+  image: text("image"),
+  createdAt: timestamp("created_at")
     .$defaultFn(() => /* @__PURE__ */ new Date())
     .notNull(),
-  updatedAt: timestamp('updated_at')
+  updatedAt: timestamp("updated_at")
     .$defaultFn(() => /* @__PURE__ */ new Date())
     .notNull(),
-  role: text('role'),
-  banned: boolean('banned'),
-  banReason: text('ban_reason'),
-  banExpires: timestamp('ban_expires')
-})
+  role: text("role"),
+  banned: boolean("banned"),
+  banReason: text("ban_reason"),
+  banExpires: timestamp("ban_expires"),
+});
 
-export const session = pgTable('session', {
-  id: text('id').primaryKey(),
-  expiresAt: timestamp('expires_at').notNull(),
-  token: text('token').notNull().unique(),
-  createdAt: timestamp('created_at').notNull(),
-  updatedAt: timestamp('updated_at').notNull(),
-  ipAddress: text('ip_address'),
-  userAgent: text('user_agent'),
-  userId: text('user_id')
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
     .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  impersonatedBy: text('impersonated_by')
-})
+    .references(() => user.id, { onDelete: "cascade" }),
+  impersonatedBy: text("impersonated_by"),
+});
 
-export const account = pgTable('account', {
-  id: text('id').primaryKey(),
-  accountId: text('account_id').notNull(),
-  providerId: text('provider_id').notNull(),
-  userId: text('user_id')
+export const account = pgTable("account", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
     .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  accessToken: text('access_token'),
-  refreshToken: text('refresh_token'),
-  idToken: text('id_token'),
-  accessTokenExpiresAt: timestamp('access_token_expires_at'),
-  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
-  scope: text('scope'),
-  password: text('password'),
-  createdAt: timestamp('created_at').notNull(),
-  updatedAt: timestamp('updated_at').notNull()
-})
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: timestamp("created_at").notNull(),
+  updatedAt: timestamp("updated_at").notNull(),
+});
 
-export const verification = pgTable('verification', {
-  id: text('id').primaryKey(),
-  identifier: text('identifier').notNull(),
-  value: text('value').notNull(),
-  expiresAt: timestamp('expires_at').notNull(),
-  createdAt: timestamp('created_at').$defaultFn(
+export const verification = pgTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").$defaultFn(
     () => /* @__PURE__ */ new Date()
   ),
-  updatedAt: timestamp('updated_at').$defaultFn(
+  updatedAt: timestamp("updated_at").$defaultFn(
     () => /* @__PURE__ */ new Date()
-  )
-})
+  ),
+});
+// user management tables
+
+// asset management tables
 
 export const category = pgTable("category", {
   id: serial("id").primaryKey(),
@@ -92,6 +105,8 @@ export const asset = pgTable("asset", {
     .$defaultFn(() => new Date())
     .notNull(),
 });
+
+// asset management tables
 
 export const payment = pgTable("payment", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -182,4 +197,38 @@ export const assetsRelations = relations(asset, ({ one, many }) => ({
     references: [category.id],
   }),
   purchases: many(purchase),
+}));
+
+export const paymentRelations = relations(payment, ({ one, many }) => ({
+  user: one(user, {
+    fields: [payment.userId],
+    references: [user.id],
+  }),
+  purchases: many(purchase),
+}));
+
+export const purchaseRelations = relations(purchase, ({ one }) => ({
+  asset: one(asset, {
+    fields: [purchase.assetId],
+    references: [asset.id],
+  }),
+  user: one(user, {
+    fields: [purchase.userId],
+    references: [user.id],
+  }),
+  payment: one(payment, {
+    fields: [purchase.paymentId],
+    references: [payment.id],
+  }),
+}));
+
+export const invoiceRelations = relations(invoice, ({ one }) => ({
+  purchase: one(purchase, {
+    fields: [invoice.purchaseId],
+    references: [purchase.id],
+  }),
+  user: one(user, {
+    fields: [invoice.userId],
+    references: [user.id],
+  }),
 }));
